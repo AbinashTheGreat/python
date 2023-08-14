a2efd7ca240a9383953242bd4b2be2e55769c708
AbinashTheGreat <email_address>
2023-08-14 12:05:28 +0545

    QA base model

--- PositionGuess.py
+++ PositionGuess.py
@@ -1,6 +1,9 @@
+# Imported string and random for the working of code
 import string , random
+#Created list of alphabet from a-z 
 guess = list (string.ascii_uppercase)
 rand = guess[random.randrange(27)]
+# Below Line of code first prints a random and Suggests the right alphabet if wrong
 print(rand)
 ans = int(input("Guess the number:  "))
 if guess[ans - 1] == rand:
