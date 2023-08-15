f32c9860abc43dddc3b88d6487f391ac5767b343
AbinashTheGreat <email_address>
2023-08-15 16:08:33 +0545

    Maybe Fixed the issue

--- PositionGuess.py
+++ PositionGuess.py
@@ -3,16 +3,6 @@ import string , random
 #Created list of alphabet from a-z 
 guess = list (string.ascii_uppercase)
 # Below Line of code first prints a random and Suggests the right alphabet if wrong
-<<<<<<< HEAD
-ans = int(input("Guess the number:  "))
-i = 3
-while i < 5:
-  print(rand)
-  if guess[ans - 1] == rand:
-    print("BINGO!")
-  else:
-    print(f"{ans} is for {guess[ans - 1]}")
-=======
 i = 4
 while i < 5: 
     rand = guess[random.randrange(27)]
@@ -22,4 +12,3 @@ while i < 5:
         print("BINGO!")
     else:
         print(f"{ans} is for {guess[ans - 1]}")
->>>>>>> 8b105c83c088a79647b86372e468bdce5d58c44c
