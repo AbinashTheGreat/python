f74869a4c491df934787371aa569ac9b1d76ec2d
AbinashTheGreat <email_address>
2023-08-20 22:11:43 +0545

    Wrote some codes

--- smallProjects/math.py
+++
@@ -1,7 +0,0 @@
-import math
-# Volume of a sphere using function
-def volume(rad): # def is for defining the function
-    vols = 4 / 3 * math.pi * rad**3
-    res = round(vols, 3)
-    print(f"The volume of sphere is {res}m\u00b3")  # \u00b3 gives cube symbol
-volume(int(input("Please enter the radius of sphere: ")))
