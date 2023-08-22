2c1eb9066bd6443ecf5a9b4490b046a9c5bbc582
AbinashTheGreat <email_address>
2023-08-22 22:37:58 +0545

    may be folder

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ name = input("What's your name? ")
 # Remove white spaces from str => name = name.strip().title()
 # To Capitalize the code => name = name.capitalize()
 # To Capitalize all the word => name = name.title()
-fname , lname = name.split()
+spliter = name.split()
+if len(spliter) == 2:
+    while len(spliter) == 2:
+        print("You dont have a Middle name, if so:", end=" ")
+        name = input("What's your name: ")
+        spliter = name.split()
+        break
 # Say hello to user
-print(f"Hello, {fname}")
+fname , mname , lname = name.split()
+print(f"So, your middle name is {mname}")
---
+++ pythonPrograms/PositionGuess.py
@@ -0,0 +1,16 @@
+# Imported string and random for the working of code
+import string , random
+#Created list of alphabet from a-z 
+guess = list (string.ascii_uppercase)
+# Below Line of code first prints a random and Suggests the right alphabet if wrong
+i = 4
+while i < 5: 
+    rand = guess[random.randrange(27)]
+    print(f"What is the correct number place for: {rand}")
+    ans = int(input("Guess the number:  "))
+    if guess[ans - 1] == rand:
+        print("BINGO!")
+    else:
+        rahul = guess.index(rand) 
+        print(f"{rahul} is for {rand}")
+        print(f"{ans} is for {guess[ans - 1]}")
